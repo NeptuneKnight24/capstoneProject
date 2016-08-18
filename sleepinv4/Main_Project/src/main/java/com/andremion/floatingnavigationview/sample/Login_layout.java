5ec582103f2f06ef076387e67f2219aa24c2799a
Jigs <bloodymerehope@gmail.com>
2016-08-18 21:12:19 +0800

    Updated Splash Screen module added some animated clouds

--- sleepinv4/Main_Project/src/main/java/com/andremion/floatingnavigationview/sample/Login_layout.java
+++ sleepinv4/Main_Project/src/main/java/com/andremion/floatingnavigationview/sample/Login_layout.java
@@ -4,14 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class Login_layout extends Activity implements View.OnClickListener{
+public class Login_layout extends Activity implements View.OnTouchListener{
     Button btn_login;
     EditText user_field,pass_field;
     TextView register_now;
@@ -24,22 +26,26 @@ public class Login_layout extends Activity implements View.OnClickListener{
         user_field = (EditText)findViewById(R.id.et_username);
         pass_field = (EditText)findViewById(R.id.et_password);
 
-        register_now = (TextView)findViewById(R.id.tv_register);
+        register_now = (TextView)findViewById(R.id.tv_text);
         register_now.setPaintFlags(register_now.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
-        btn_login.setOnClickListener(this);
-        register_now.setOnClickListener(this);
-
+        btn_login.setOnTouchListener(this);
+        register_now.setOnTouchListener(this);
     }
 
 
     @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.login_btn:
-                startActivity(new Intent(Login_layout.this,MainActivity.class));
-            case R.id.tv_register:
-                startActivity(new Intent(Login_layout.this,Registration.class));
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        //test if a button has been touch
+       switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_UP:
+                //test if what button has been touch
+                if (view.getId() == R.id.login_btn) {
+                    startActivity(new Intent(Login_layout.this,MainActivity.class));
+                }else if(view.getId() == R.id.tv_text) {
+                    startActivity(new Intent(Login_layout.this,Registration.class));
+                }
         }
+        return true;
     }
 }
--- sleepinv4/Main_Project/src/main/java/com/andremion/floatingnavigationview/sample/SplashScreen.java
+++ sleepinv4/Main_Project/src/main/java/com/andremion/floatingnavigationview/sample/SplashScreen.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
@@ -15,8 +16,35 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.splash_screen_layout);
 
         ImageView fadeOutImage = (ImageView) findViewById(R.id.iv_logo);
+        ImageView cloudone = (ImageView) findViewById(R.id.iv_cloud_one);
+        ImageView cloudtwo = (ImageView) findViewById(R.id.iv_cloud_two);
+        ImageView cloudthree = (ImageView) findViewById(R.id.iv_cloud_three);
+        ImageView cloudfour = (ImageView) findViewById(R.id.iv_cloud_four);
+        ImageView cloudfive = (ImageView) findViewById(R.id.iv_cloud_five);
+        ImageView cloudsix = (ImageView) findViewById(R.id.iv_cloud_six);
+
+
         Animation startFadeInAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
         fadeOutImage.startAnimation(startFadeInAnimation);
+
+        TranslateAnimation anim_one = new TranslateAnimation(-100f, 10f, 0f, 0f);  // might need to review the docs
+        anim_one.setDuration(5500); // set how long you want the animation
+        cloudone.setAnimation(anim_one);
+        TranslateAnimation anim_two = new TranslateAnimation(-100f, 50f, 0f, 0f);  // might need to review the docs
+        anim_two.setDuration(5500);
+        cloudtwo.setAnimation(anim_two);
+        TranslateAnimation anim_three= new TranslateAnimation(-300f, 10f, 0f, 0f);  // might need to review the docs
+        anim_three.setDuration(5500);
+        cloudthree.setAnimation(anim_three);
+        TranslateAnimation anim_four= new TranslateAnimation(250f, 25f, 0f, 0f);  // might need to review the docs
+        anim_four.setDuration(5500);
+        cloudfour.setAnimation(anim_four);
+        TranslateAnimation anim_five= new TranslateAnimation(350f, 20f, 0f, 0f);  // might need to review the docs
+        anim_five.setDuration(5500);
+        cloudfive.setAnimation(anim_five);
+        TranslateAnimation anim_six= new TranslateAnimation(450f, 40f, 0f, 0f);  // might need to review the docs
+        anim_six.setDuration(5500);
+        cloudsix.setAnimation(anim_six);
 
           new Handler().postDelayed(new Runnable() {
               @Override
@@ -24,7 +52,7 @@ public class SplashScreen extends AppCompatActivity {
                   startActivity(new Intent(SplashScreen.this,Login_layout.class));
                   finish();
               }
-          },5000);
+          },4700);
     }
 
 
